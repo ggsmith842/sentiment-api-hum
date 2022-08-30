d0ba5abc33335923790da28c467fc2ebd0868129
Grant Smith <57376089+ggsmith842@users.noreply.github.com>
2022-08-30 17:27:23 -0600

    Update twitterapi.py

--- app/twitterapi.py
+++ app/twitterapi.py
@@ -11,7 +11,7 @@ def searchTweets(search_term:str):
 
   payload={}
   headers = {
-    'Authorization':'Bearer AAAAAAAAAAAAAAAAAAAAAJ2PcgEAAAAA5ZEfCVrAoDuegv5hydVOXImfEGA%3DFmEpLlJQ1Q6F8kmEuhBb1ab0ynYytfqwCwwDiQrrjpj9LbhELP',
+    'Authorization':os.getenv('BEARER_TOKEN'),
     'Cookie': 'guest_id=v1%3A165292464833657250; guest_id_ads=v1%3A165292464833657250; guest_id_marketing=v1%3A165292464833657250; personalization_id="v1_D50leSEsdlQN9nTvwQ6B+g=="'
     }
   response = requests.request("GET", url, headers=headers, data=payload).json()['includes']['tweets']
